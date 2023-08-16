338d0e3fc90ce20af64f1cdfd980f79646c4ee8e
Facundoheck <heckfacundo9@gmail.com>
2023-08-16 16:35:39 -0300

    commit segundo metodo

--- PyBank/Resources/main.py
+++ PyBank/Resources/main.py
@@ -13,14 +13,25 @@ def openFile(archivo):
 #guardo en una variable "data" la lista con los datos del archivo
 data=openFile('budget_data.csv')
 
-#inicializo las diferentes listas que voy a extraer de "data" 
+#inicializo la lista que voy a extraer de "data" 
 fecha=[]
-profitorlosses=[]
 for i in data:
     fecha.append(i[0])
-    profitorlosses.append(i[1])
 #elimino el indice de la lista
 fecha.pop(0)
 #cuento la cantidad de datos que existen en la lista (sin contar el indice)
 count=sum(1 for i in fecha)
 print('Total months: ', count)
+
+######################################################################################################
+
+#inicializo la lista que voy a extraer de "data" 
+profitorlosses=[]
+for i in data:
+    profitorlosses.append(i[1])
+#elimino el indice de la lista
+profitorlosses.pop(0)
+#convierto los datos de la lista a enteros, los sumo y los guardo en una nueva variable "total"
+total=sum(list(map(int,profitorlosses)))
+
+print('Total: $',total)
