b8f705982419f6fedb9d2d76084fe95879623de7
Facundoheck <heckfacundo9@gmail.com>
2023-08-16 16:25:27 -0300

    commit primer metodo modificado

--- PyBank/Resources/main.py
+++ PyBank/Resources/main.py
@@ -1,13 +1,26 @@
 import csv
 
-#funcion que devuelve la cantidad de meses que esten en el archivo budget_data.csv
-def datosSinEncabezado(archivo):
-  with open(archivo,'r') as apertura:
-    lectura=csv.reader(apertura)
-    next(lectura)
-    count=sum(1 for i in lectura)
-  return count
+#abre un archivo csv, convierte el archivo en una lista con encabezados
+def openFile(archivo):
+    global lista
+    with open(archivo,'r') as apertura:
+        lectura=csv.reader(apertura)
+        lista=[]
+        for i in lectura:
+            lista.append(i)
+    return lista
 
-data=datosSinEncabezado('budget_data.csv')
-print('Total months: ',data)
+#guardo en una variable "data" la lista con los datos del archivo
+data=openFile('budget_data.csv')
 
+#inicializo las diferentes listas que voy a extraer de "data" 
+fecha=[]
+profitorlosses=[]
+for i in data:
+    fecha.append(i[0])
+    profitorlosses.append(i[1])
+#elimino el indice de la lista
+fecha.pop(0)
+#cuento la cantidad de datos que existen en la lista (sin contar el indice)
+count=sum(1 for i in fecha)
+print('Total months: ', count)
