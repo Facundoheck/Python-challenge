c9f89272d81d7f00709c75720921a61d5badd9eb
Facundo Heck <heckfacundo9@gmail.com>
2023-08-17 14:24:18 -0300

    commit segunda solucion

--- PyPoll/Resources/main.py
+++ PyPoll/Resources/main.py
@@ -21,3 +21,38 @@ print_total_votes=f"Total votes: {votos}"
 
 #----------------------------------------------------------------------
 
+#inicializo las listas de los votantes
+Charles=[]
+Diana=[]
+Raymon=[]
+Nulos=[]
+
+#por cada voto, se suma un registro (Ballot ID,County,Candidate) a la lista
+for i in lista:
+    if i[2]=='Charles Casper Stockham':
+        Charles.append(i)
+    elif i[2]=='Diana DeGette':
+        Diana.append(i)
+    elif i[2]=='Raymon Anthony Doane':
+        Raymon.append(i)
+    else:
+        Nulos.append(i)
+
+#cantidad de votos de cada candidato y tambien los votos nulos o anulados
+charles_votes=len(Charles)
+diana_votes=len(Diana)
+raymon_votes=len(Raymon)
+nulos_votes=len(Nulos)
+
+#funcion para definir el porcentaje de votos que obtuvo cada candidato
+def Porcentajes(cant_votos):
+    global percent
+    total_percent=(cant_votos*100)/votos
+    percent=round(total_percent,2)
+    return percent
+
+#porcentaje de votos que obtuvo cada candidato
+percent_charles_votes=Porcentajes(charles_votes)
+percent_diana_votes=Porcentajes(diana_votes)
+percent_raymon_votes=Porcentajes(raymon_votes)
+percent_nulos_votes=Porcentajes(nulos_votes)
